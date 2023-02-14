e99586cb2819c7ab9600f730c678c38cc89a81fc
sterling_hebert <sterling_hebert@outlook.com>
2023-02-13 19:56:41 -0600

    added reviews model and migration

---
+++ backend/db/models/review.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Review.init(
+    {
+      spotId: { type: DataTypes.INTEGER, allowNull: false },
+      userId: { type: DataTypes.INTEGER, allowNull: false },
+      review: DataTypes.STRING,
+      stars: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Review",
+      defaultScope: {
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      },
+    }
+  );
+  return Review;
+};
