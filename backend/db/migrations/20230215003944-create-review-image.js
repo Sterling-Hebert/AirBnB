03a800d2e9591ab9035c4910cf0328bbc2547d65
sterling_hebert <sterling_hebert@outlook.com>
2023-02-15 17:01:49 -0600

    fixed options error with drop column

--- backend/db/migrations/20230215003944-create-review-image.js
+++ backend/db/migrations/20230215003944-create-review-image.js
@@ -37,7 +37,6 @@ module.exports = {
     );
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = "ReviewImages";
-    await queryInterface.dropTable(options);
+    await queryInterface.dropTable("ReviewImages", options);
   },
 };
